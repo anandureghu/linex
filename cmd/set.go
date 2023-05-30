7a6d7a8362cd6a0a03fc8fe82b91484fa125e717
qb-anandureghu <anandu.reghu@qburst.com>
2023-05-30 21:59:52 +0530

    added description

--- cmd/set.go
+++ cmd/set.go
@@ -13,14 +13,9 @@ import (
 
 // setCmd represents the set command
 var setCmd = &cobra.Command{
-	Use:   "set",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "set <command name> script_1 script_2 .... script_n",
+	Short: "add or update command and scripts",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		commands := helper.ReadJSONToken()
